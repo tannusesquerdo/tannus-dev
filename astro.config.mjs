eb6879fdbee92c77ef42d6d4cb5744045b58ea59
Tannus Esquerdo <tandrad8@my.centennialcollege.ca>
2024-01-17 21:08:09 -0500

    add gtag

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,19 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import partytown from "@astrojs/partytown";
+
+import sitemap from "@astrojs/sitemap";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
